581e104b7c8eebbf4b2c6b5278ad478da404ea55
Miroslaw Mikula <info@softmed.pl>
2018-08-24 18:52:22 +0200

    initial commit

---
+++ src/main/java/com/project/sda/timetable/TimetableApplication.java
@@ -0,0 +1,12 @@
+package com.project.sda.timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimetableApplication.class, args);
+    }
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE group(
+	id bigint(20) NOT NULL AUTO_INCREMENT,
+	name varchar(50) NOT NULL,
+  code_name varchar(5) NOT NULL,
+  PRIMARY KEY (id)
+)ENGINE=MyISAM DEFAULT CHARSET=utf8;
